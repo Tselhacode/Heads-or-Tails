2422048a5703ee98a948935122dee5c2ff480a37
Tselhacode <68340816+Tselhacode@users.noreply.github.com>
2021-07-19 07:52:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+random_toss = random.randint(0,1)
+if random_toss == 1:
+  print("Heads")
+else:
+  print("Tail")
+
+
+
+
+
+
+
+
+
+
